bc230d299b4874d583342a50668cebd1db4cb2c9
Bernard <bh20@hw.ac.uk>
2020-10-29 20:52:31 +0000

    Learning tensorflow

---
+++ Tensor_flow ex/Classification.py
@@ -0,0 +1,62 @@
+#used to seperate data points into classes of different labels
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf 
+import pandas as pd
+
+#Dataset
+
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file(
+    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+#use keras to grab our datasets and read them into pandas dataframe
+
+train.head()
+train_y = train.pop('Species')
+test_y = test.pop('Species')
+train.head() #the species column is not gone
+#train.shape()
+
+#input function
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+    
+    return dataset.batch(batch_size)
+
+#feature columns
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+print(my_feature_columns)
+
+#building the model
+#Build a DNN with 2 hidden layers with 30 and 10 hidden nodes each
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    # Two hidden layers of 30 and 10 nodes respectively.
+    hidden_units=[30, 10],
+    # The model must choose between 3 classes.
+    n_classes=3)
+
+#training time!
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+
+eval_result = classifier.evaluate(
+    input_fn=lambda: input_fn(test, test_y, training=False))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
