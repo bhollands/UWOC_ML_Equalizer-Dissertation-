414702e14a38fdd79f5fea105d1a7e945bc506f6
Bernard <bh20@hw.ac.uk>
2020-11-10 21:19:08 +0000

    Made initial NN

--- MLP/mpl_equalizer.py
+++ MLP/mpl_equalizer.py
@@ -5,6 +5,11 @@ from sklearn.model_selection import train_test_split #to make a % of the dataset
 import tensorflow as tf
 from tensorflow import keras
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+#need to make train set and test set
 
 in_file = os.path.dirname(os.path.abspath(__file__)) #fix later
 Rx_datapath = os.path.join(in_file, 'PAMsymRx.csv')
@@ -17,27 +22,48 @@ PAMsymRx = pd.read_csv(Rx_datapath)
 PAMsymTx_Array = PAMsymTx.values #turn value into usable numpy array
 PAMsymRx_Array = PAMsymRx.values
 
-PAMsymTx_Array.flatten() #make the arrays 1D and easier to use
-PAMsymRx_Array.flatten()
+Tx_1D = PAMsymTx_Array.flatten() #make the arrays 1D and easier to use
+Rx_1D = PAMsymRx_Array.flatten()
+
+Tx_split = np.array_split(Tx_1D, 2)
+Tx_train = Tx_split[0]
+Tx_test = Tx_split[1]
+
+Rx_split = np.array_split(Rx_1D, 2)
+Rx_train = Rx_split[0]
+Rx_test = Rx_split[1]
 
 
-PAMsymRx_Array, _, PAMsymTx, _ = train_test_split( #option of only using % of the dataset for faster testing
-    RX_sm, TX_sm,  test_size = 0.9
+Rx_train_sm, _, Tx_train_sm, _ = train_test_split( #option of only using % of the dataset for faster testing
+    Rx_train,Tx_train, test_size = 0.9
 )
 
-
-model = keras.Sequential ([
-    keras.layers.Dense(1)
-    keras,layers.Dense(128, activation='relu')
-    keras,layers.Dense(2, activation='relu')
-])
-
-model.compile(
-    optimizer = 'adam',
-    loss = 'sparse_categorical_crossentropy',
-    metrics=['accuracy']
+Rx_test_sm, _, Tx_test_sm, _ = train_test_split( #option of only using % of the dataset for faster testing
+    Rx_test,Tx_test, test_size = 0.9
 )
 
-model.fit(train_Rx, train_Tx, epochs=10)
-test_loss, test_acc = model.evaluate(test_Rx, Test_Tx, verbose=0)
-print('Test accuracy', test_acc)
+batch_size = 128
+num_classes = 2
+epochs = 30
+
+model = Sequential()
+
+model.add(Dense(2, activation='sigmoid'))
+model.add(Dropout(0.1))
+model.add(Dense(3, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(num_classes, activation='softmax'))
+
+#model.summary()
+
+model.compile(loss='mse', optimizer=RMSprop(), metrics=['accuracy'])
+history = model.fit(Rx_train_sm, Tx_train_sm,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(Rx_test_sm, Tx_test_sm))
+score = model.evaluate(Rx_test_sm,Tx_test_sm, verbose=0)
+print('Test loss:', score[0])
+print('Test Accuracy:', score[1])
+
+
