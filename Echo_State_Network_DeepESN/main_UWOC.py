fc6d830e2c83d2c168f5009ad4066dcf23ced8d7
Bernard <bh20@hw.ac.uk>
2021-05-23 11:00:53 +0100

    Final Push 3

--- Echo_State_Network_DeepESN/main_UWOC.py
+++ Echo_State_Network_DeepESN/main_UWOC.py
@@ -28,9 +28,13 @@ import numpy as np
 import pandas as pd
 import random
 from DeepESN import DeepESN
-from scipy.io import loadmat
-from utils import MSE, config_MG, load_UWOC, select_indexes 
+from scipy.io import loadmat, savemat
+from utils import MSE, config_UWOC, load_UWOC, select_indexes, MAPE
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import time
+import itertools
+
 class Struct(object): pass
 
 def saveOutputResults(predict):
@@ -51,79 +55,115 @@ def plotResults(predict):
     plt.show()
 
 
-def roundTo(array):
-    for i in range(array.size):
-        if array[i] > 0:
-            array[i] = 1
-        else:
-            array[i] = -1
-    return array
-
-def ber(toCheck, perfect):
-    error = 0
-    for i in range(perfect.size):
-        if toCheck[i] != perfect[i]:
-            error = error +1
-    ber = error/perfect.size
-    return ber
-        
-
-
-
 def main():
 
     # fix a seed for the reproducibility of results
     np.random.seed(7)
-
-    # dataset path 
-    path = 'Echo_State_Network_DeepESN\datasets'
-    dataset, Nu, error_function, optimization_problem, TR_indexes, VL_indexes, TS_indexes = load_UWOC(path, MSE)
-
-    # load configuration for pianomidi task
-    configs = config_MG(list(TR_indexes) + list(VL_indexes))
-    # Be careful with memory usage
-    Nr = 5 # number of recurrent units
-    Nl = 50 # number of recurrent layers
-    reg = 0.0
+    path = ''
+    dataset, Nu, error_function, optimization_problem, TR_indexes, VL_indexes, TS_indexes, scaler, data_set_name = load_UWOC(path, MSE)
+ 
+    # load configuration for UWOC task
+    config = config_UWOC(list(TR_indexes) + list(VL_indexes))
+    # Be careful with memory usage dummy
+    recc_units = 10 # number of recurrent units
+    recc_layer = 17 # number of recurrent layers
+     
+    reg = 0
     transient = 100
-    deepESN = DeepESN(Nu, Nr, Nl, configs)
+    deepESN = DeepESN(Nu, recc_units, recc_layer, config)
+
     states = deepESN.computeState(dataset.inputs, deepESN.IPconf.DeepIP)
+
     train_states = select_indexes(states, list(TR_indexes) + list(VL_indexes), transient)
     train_targets = select_indexes(dataset.targets, list(TR_indexes) + list(VL_indexes), transient)
     
-    test_states = select_indexes(states, TS_indexes)
     test_targets = select_indexes(dataset.targets, TS_indexes)
-    
-    deepESN.trainReadout(train_states, train_targets, reg)
+    start_time_tr = time.time()
+    deepESN.trainReadout(train_states, train_targets, reg) #train the network
+    end_time_tr = time.time() - start_time_tr
+    print('Training Time:', end_time_tr)
     
     train_outputs = deepESN.computeOutput(train_states)
-    train_error = error_function(train_outputs, train_targets)
+
+
+    train_error = error_function(X = train_outputs,Y = train_targets)
     print('Training MSE: ', np.mean(train_error), '\n')
+
+    start_time = time.time()
+    test_states = select_indexes(states, TS_indexes)
     test_outputs = deepESN.computeOutput(test_states)
-   
+    end_time = time.time() - start_time
+
     
-    test_error = error_function(test_outputs, test_targets)
+    test_error = error_function(X = test_outputs, Y = test_targets)
     print('Test MSE: ', np.mean(test_error), '\n')
-    saveOutputResults(test_outputs)
+    print("Time to put unseen data through : %s seconds" % (end_time))
+ 
 
     test_targets = np.array(test_targets)
+    test_targets = test_targets.flatten()
 
-    test_outputs = roundTo(test_outputs.flatten())
+    test_states = np.array(test_states)
+    test_states = test_states.flatten()
 
-    test_error_after = error_function(test_outputs, test_targets)
-    print("After rounding:", test_error_after)
+    from scipy.io import savemat, loadmat
+    #test_outputs
+    #test_targets
+    myData = loadmat(data_set_name) 
+    orig_Rx = myData['PAMsymRx']#.reshape((1,-1))#Mat']#.reshape((1,-1))
+    orig_Tx = myData['PAMsymTx']#.reshape((1,-1))#Mat']#.reshape((1,-1))
+   
+    equalized = test_outputs.reshape(-1,1)
+    tr = 20000
+    Tx = orig_Tx[tr:240000].reshape(-1,1)
+    Input = orig_Rx[tr:240000].reshape(-1,1)
 
-    bitErrorRate = ber(test_outputs.flatten(), test_targets.flatten())
-    print("BER: ", bitErrorRate)
+    equalized = scaler.inverse_transform(equalized)
 
-    plt.plot(test_targets.flatten(), 'b-o', markersize = 4, label="Ideal ")
-    plt.plot(test_outputs.flatten(), 'r-o', markersize = 4, label="Output")
-    #plt.plot(test_states, 'g-o', markersize = 4, label="Input")
+    mdic = {"Rx": equalized, "Tx": Tx ,"input":Input}
+    savemat("equalizedOutput.mat", mdic)
+
+    from sklearn.metrics import classification_report
+    from sklearn.metrics import confusion_matrix
+    import scikitplot as skplt
+    import pandas as pd
+    import seaborn as sns
+    
+    plt.plot(Tx.flatten(), 'r-o', markersize = 4, label="Tx")
+    plt.plot(equalized.flatten(), 'b-o', markersize = 4, label="Equalized Rx")
+    plt.plot(Input.flatten(), 'g-o', markersize = 4, label="Rx")
     plt.legend(loc='upper right')
-    plt.xlabel("Number of bit")
-    plt.ylabel("Bit status")
+    plt.xlabel("Number of Symbol")
+    plt.ylabel("Symbol Status")
     plt.xlim([0,50])
-    plt.show()
+    plt.grid()
+    #plt.show()
+
+    #print("Layers:", Nl)
+    #back_to_original(test_outputs_classified)
+
+    # test_targets_orig = back_to_original(test_targets)
+    # class_names = ["8191", "2730","-2730","-8191"]
+    # label_numbers = [8191,2730,-2730,-8191]
+    #class_names = ["0", "1"]
+    #label_numbers = [0,1]
+
+    #print(classification_report)
+
+
+    #report = classification_report(test_targets, test_outputs_classified, digits = 5, target_names=class_names, output_dict = False)
+
+    #print(report)
+    #sns.heatmap(pd.DataFrame(report).iloc[:-1, :].T, annot=True, linewidths=.5)
+    #plot_classification_report(report)
+
+
+    # print("Confusion Matrix")
+    # print(print(confusion_matrix(test_targets, test_outputs_classified, labels = label_numbers)))
+
+    # skplt.metrics.plot_confusion_matrix(test_targets, test_outputs_classified, normalize = False, cmap=plt.cm.Blues)
+    
+    
 
 if __name__ == "__main__":
     main()
--- MLP_with_PSO/src/main.py
+++ MLP_with_PSO/src/main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
 
 '''
 saves the results of a run
@@ -36,21 +38,25 @@ def saveResults(predict, actual, activeFunc, file, fitness):
 configures some aspects of the network
 '''
 def configure(file_select):
-    
+    scaler = MinMaxScaler(feature_range=(0,1))
     if file_select == "uwoc":
         myData = loadmat('POF60m_PAMExp_2PAM_DR600Mbps(single column).mat')   
         Rx = myData['PAMsymRx']
         Tx = myData['PAMsymTx']
-        X = Rx[0:20000]#1004040] #set to 1004040 for full dataset
-        y = Tx[0:20000]#1004040]
+        X = Rx[0:2000]#1004040] #set to 1004040 for full dataset
+        y = Tx[0:2000]#1004040]
+        X = scaler.fit_transform(X)
+        y = scaler.fit_transform(y)
         #X = X/y
         single_input = True
     else: 
         data = data_reader() #Create new data reader
         single_input = data.select(file_select) #select the file and it returns if it is single input
         X = data.input_array() #input input array
+        
         print(X)
         y = data.expected_output_array() #get function output
+        
 
     if single_input: #if network is single input one input we only need single input neuron
         input_layer = 1
@@ -77,7 +83,7 @@ def singleRun(file, activationFunc, hiddenLayersSize, numOfHidden, save, epochs,
         #saveFitness(result[1], file)
 
 def main():
-    singleRun(file="uwoc", activationFunc="cosine", hiddenLayersSize = 5, 
+    singleRun(file="uwoc", activationFunc="cosine", hiddenLayersSize = 40, 
                 numOfHidden = 2, save=True, epochs=50, numParticels=200,  
                 posRange=(-2.0,2.0), velRange=(-2.0, 2.0), 
                 coef = (0.5,0.3), iWeightRange = (0.9, 0.9))
